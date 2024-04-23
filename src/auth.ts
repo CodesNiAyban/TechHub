8834c964d6792161cb4ad0250391e137fff88e24
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-04-24 05:41:26 +0800

    Initial OAuth

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,21 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import prisma from "./lib/prisma"
- 
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import { Adapter } from "next-auth/adapters"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [],
+  trustHost: true,
+  theme: {
+    logo: "/logo.png"
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    }
+  },
+  providers: [Google, Github],
 })
